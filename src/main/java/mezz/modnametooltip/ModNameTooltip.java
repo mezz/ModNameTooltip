2c4fac4d0cb8a3ca91e7e0571ef28a39b162efa5
mezz <tehgeek@gmail.com>
2015-12-10 23:53:32 -0800

    Build for 1.8.8

--- src/main/java/mezz/modnametooltip/ModNameTooltip.java
+++ src/main/java/mezz/modnametooltip/ModNameTooltip.java
@@ -1,45 +1,29 @@
 package mezz.modnametooltip;
 
-import javax.annotation.Nonnull;
-import java.util.Map;
-
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.FMLLog;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLModDisabledEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.network.NetworkCheckHandler;
-import net.minecraftforge.fml.relauncher.Side;
 
 @Mod(
 		modid = ModNameTooltip.MODID,
 		version = ModNameTooltip.VERSION,
 		acceptedMinecraftVersions = "[1.8,1.8.9]",
-		clientSideOnly = true,
-		canBeDeactivated = true
+		clientSideOnly = true
 )
 public class ModNameTooltip {
 	public static final String MODID = "ModNameTooltip";
-	public static final String VERSION = "1.0";
-
-	private TooltipEventHandler tooltipEventHandler;
-
-	@NetworkCheckHandler
-	public boolean checkModLists(Map<String, String> modList, Side side) {
-		return !modList.containsKey("Waila");
-	}
+	public static final String VERSION = "@VERSION@";
 
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
-		tooltipEventHandler = new TooltipEventHandler();
-		MinecraftForge.EVENT_BUS.register(tooltipEventHandler);
-	}
-
-	@SubscribeEvent
-	public void onDeactivate(@Nonnull FMLModDisabledEvent event) {
-		if (tooltipEventHandler != null) {
-			MinecraftForge.EVENT_BUS.unregister(tooltipEventHandler);
+		if (Loader.isModLoaded("Waila")) {
+			FMLLog.warning("Waila detected. It also adds the Mod Name to the Tooltip. Deactivating " + MODID + '.');
+			return;
 		}
+		TooltipEventHandler tooltipEventHandler = new TooltipEventHandler();
+		MinecraftForge.EVENT_BUS.register(tooltipEventHandler);
 	}
 }
