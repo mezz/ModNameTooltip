041c0871094351e061376fe745dd6263fcaf3d8d
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-06-11 12:15:44 +0200

    it loads

--- src/main/java/mezz/modnametooltip/ModNameTooltipClient.java
+++ src/main/java/mezz/modnametooltip/ModNameTooltipClient.java
@@ -10,21 +10,21 @@ import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.config.ModConfigEvent;
+import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 
 import java.util.function.Consumer;
 
 @Mod("modnametooltip")
-@OnlyIn(Dist.CLIENT)
 public class ModNameTooltipClient {
-	public ModNameTooltipClient(IEventBus eventBus) {
+	public ModNameTooltipClient() {
 		Config config = new Config();
 		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, config.getConfigSpec());
 		TooltipEventHandler tooltipEventHandler = new TooltipEventHandler(config);
+		IEventBus eventBus = NeoForge.EVENT_BUS;
 		addListener(eventBus, ModConfigEvent.class, EventPriority.NORMAL, config::onConfigChanged);
 		addListener(eventBus, ItemTooltipEvent.class, EventPriority.LOW, tooltipEventHandler::onToolTip);
 	}
-
 	private static <T extends Event> void addListener(IEventBus eventBus, Class<T> eventType, EventPriority priority, Consumer<T> listener) {
 		eventBus.addListener(priority, false, eventType, listener);
 	}
