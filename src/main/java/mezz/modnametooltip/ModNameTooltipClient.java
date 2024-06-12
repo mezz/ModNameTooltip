009fe735bb96c69905e12c39cd932ddfb4c21843
mezz <tehgeek@gmail.com>
2024-06-12 10:27:12 +0900

    cleanup

--- src/main/java/mezz/modnametooltip/ModNameTooltipClient.java
+++ src/main/java/mezz/modnametooltip/ModNameTooltipClient.java
@@ -1,8 +1,5 @@
 package mezz.modnametooltip;
 
-
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.Event;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.IEventBus;
@@ -25,6 +22,7 @@ public class ModNameTooltipClient {
 		addListener(modBus, ModConfigEvent.class, EventPriority.NORMAL, config::onConfigChanged);
 		addListener(eventBus, ItemTooltipEvent.class, EventPriority.LOW, tooltipEventHandler::onToolTip);
 	}
+	
 	private static <T extends Event> void addListener(IEventBus eventBus, Class<T> eventType, EventPriority priority, Consumer<T> listener) {
 		eventBus.addListener(priority, false, eventType, listener);
 	}
